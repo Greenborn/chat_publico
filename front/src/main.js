3013b67d0cf34473529e41ed954b6547c029c672
luciano <lucho.2012.tandil@gmail.com>
2025-08-05 23:26:39 -0300

    Solo Bootstrap puro, estilos de botones verde oscuro y limpieza de dependencias

--- front/src/main.js
+++ front/src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+
 import BootstrapIcon from '@dvuckovic/vue3-bootstrap-icons'
 
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+import './assets/dark-greenborn.css'
+import './assets/custom-greenborn.css'
 
 let vue = createApp(App)
 vue.component('BootstrapIcon', BootstrapIcon);
